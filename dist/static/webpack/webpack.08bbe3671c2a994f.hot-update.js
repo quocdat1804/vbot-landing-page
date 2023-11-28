0373d0a420c728b50755f6a8b453d27a46340421
qdatt_macmini <datnguyenquoc1804@gmail.com>
2023-11-28 14:54:43 +0700

    update favicon

--- dist/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ dist/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e9e356012448ca7a"; }
+/******/ 	__webpack_require__.h = function() { return "28f0c270b8fbcd67"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
